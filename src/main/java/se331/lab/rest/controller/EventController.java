10a4bf8e3bd7f47aeeedbbcb58b299652d185900
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-09-03 13:53:50 +0700

    3.3

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,10 +1,13 @@
 package se331.lab.rest.controller;
 
 import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -112,6 +115,7 @@ public class EventController {
                 .organizer("Brody Kill")
                 .build());
     }
+
     @GetMapping("events")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit",
             required = false) Integer perPage
@@ -120,9 +124,31 @@ public class EventController {
         page = page == null ? 1 : page;
         Integer firstIndex = (page-1) * perPage;
         List<Event> output = new ArrayList<>();
+        try{
         for (int i = firstIndex; i <= firstIndex + perPage; i++) {
             output.add(eventsList.get(i));
         }
         return ResponseEntity.ok(output);
+    }catch (IndexOutOfBoundsException ex){
+        }
+        return ResponseEntity.ok(output);
     }
-}
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+        Event output = null;
+        for (Event event :
+                eventsList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
+            }
+        }
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        }else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "The given id is not found");
+            }
+        }
+    }
