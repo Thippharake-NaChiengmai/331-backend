a19be8dd9643f5b4de7a04ec377a1b312daa37eb
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-09-03 14:59:23 +0700

    4.6

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -18,10 +18,9 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events")
-                        .allowedOrigins("http://localhost:5173");
-                registry.addMapping("/events/*")
-                        .allowedMethods("http://localhost:5173");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
             }
         };
     }
