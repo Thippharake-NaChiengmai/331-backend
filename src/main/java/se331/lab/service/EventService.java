ebf13dde1c849616162bcd9e9f51778e6290eb68
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-09-03 19:18:48 +0700

    5.7

--- src/main/java/se331/lab/service/EventService.java
+++ src/main/java/se331/lab/service/EventService.java
@@ -1,4 +1,10 @@
 package se331.lab.service;
 
-public class EventService {
+import se331.lab.entity.Event;
+import java.util.List;
+
+public interface EventService {
+    Integer getEventSize();
+    List<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Integer id);
 }
